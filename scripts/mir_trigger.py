f8dce9fa5f660febc6e5f4626b42e623d89443f6
Marcus Myllyviita <marcus.myllyviita@aalto.fi>
2022-04-04 12:09:36 +0300

    Added node for triggering mir

---
+++ scripts/mir_trigger.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String, Bool
+from move_base_msgs.msg import MoveBaseActionGoal
+
+
+goal_x, goal_y = 11.3160, 12.0000
+orient_z, orient_w = 0.7007, 0.9543
+
+class Mir_Trigger():
+
+    def __init__(self):
+        self.pickup_goal = MoveBaseActionGoal()
+        self.pickup_goal.goal.target_pose.pose.position.x = goal_x
+        self.pickup_goal.goal.target_pose.pose.position.y = goal_y
+        self.pickup_goal.goal.target_pose.pose.orientation.x = orient_z
+        self.pickup_goal.goal.target_pose.pose.orientation.y = orient_w
+
+        rospy.init_node('mir_trigger')
+
+        self.pub = rospy.Publisher('/move_base/goal', MoveBaseActionGoal, queue_size=10)
+        rospy.Subscriber('/pickup', Bool, self._callback)
+        rospy.loginfo("Subscribing to /pickup")
+        rospy.spin()
+
+    def _callback(self, data):
+        rospy.loginfo("{}: I heard {}".format(rospy.get_caller_id(), data.data))
+        pickup = data.data
+        if pickup:
+            self.pub.publish(self.pickup_goal)
+            rospy.loginfo("Proceeding to move MiR to goal:\n {}".format(self.pickup_goal.goal.target_pose.pose.position))
+
+if __name__ == '__main__':
+    Mir_Trigger()
